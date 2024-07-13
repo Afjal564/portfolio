936cf5add3e97016fdd9cb70d21317cec6aafc26
afjal <afjalkhan564786@gmail.com>
2024-07-13 23:17:28 +0530

    project added

---
+++ src/constants/index.js
@@ -0,0 +1,465 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  tc,
+  job,
+  ez,
+  jobit,
+  cricle,
+  Right,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Wordpress Developer",
+    icon: backend,
+  },
+  {
+    title: "Full stack Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Rightbraininfotech",
+    icon: cricle,
+    iconBg: "#383E56",
+    points: [
+      "Developed dynamic and responsive web applications using React.js.",
+      "CCreated and maintained reusable components and libraries to ensure code modularity and maintainability.",
+      "Collaborated with designers and backend developers to integrate APIs and ensure seamless user experiences.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Rightbraininfotech",
+    icon: cricle,
+    iconBg: "#E6DEDD",
+  
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Rightbraininfotech",
+    icon: cricle,
+    iconBg: "#383E56",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Rightbraininfotech",
+    icon:cricle,
+    iconBg: "#E6DEDD",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+ 
+  {
+    name: "Drivers In India",
+    description:
+      "Drivers In India is a platform which provide services like Driver on Hire, Temporary/ permanent driving services. Users can book the services through a mobile/ web app.",
+      tasks: 
+       
+           "Converting Design Systems into reusable CSS and Components  Making complex layouts Developing and maintaining the user interfaces  Worked on Optimization, Performance and UX Improvement. Making Responsive UI. Integrating API s and RESTful Services."
+       
+      ,
+     
+      tags: [
+        {
+          id: 0,
+          name: "HTML"
+        },
+        {
+          id: 1,
+          name: "SCSS"
+        },
+        {
+          id: 2,
+          name: "JavaScript"
+        },
+        {
+          id: 3,
+          name: "Angular"
+        },
+        {
+          id: 3,
+          name: "Ionic"
+        },
+        {
+          id: 5,
+          name: "Figma"
+        },
+        {
+          id: 6,
+          name: "Responsive Design"
+        },
+        {
+          id: 7,
+          name: "REST"
+        },
+        {
+          id: 8,
+          name: "Tailwindcss"
+        },
+        {
+          id: 9,
+          name: "Material UI"
+        }
+      ],
+    image: carrent,
+    source_code_link: "https://driversinindia.com/",
+  }, {
+    name: "RightBrain Infotech",
+    description:
+    "RightBrain Infotech is a dynamic IT company specializing in software development, including web and mobile application development, cloud computing, and IT consulting services.",
+    tasks: 
+       
+    "Converting Design Systems into reusable CSS and Components  Making complex layouts Developing and maintaining the user interfaces  Worked on Optimization, Performance and UX Improvement. Making Responsive UI. Integrating API s and RESTful Services.",
+    tags: [
+      {
+        id: 0,
+        name: "HTML"
+      },
+      {
+        id: 1,
+        name: "SCSS"
+      },
+      {
+        id: 2,
+        name: "JavaScript"
+      },
+      {
+        id: 3,
+        name: "Angular"
+      },
+      {
+        id: 3,
+        name: "Ionic"
+      },
+      {
+        id: 5,
+        name: "Figma"
+      },
+      {
+        id: 6,
+        name: "Responsive Design"
+      },
+      {
+        id: 7,
+        name: "REST"
+      },
+      {
+        id: 8,
+        name: "Tailwindcss"
+      },
+      {
+        id: 9,
+        name: "Material UI"
+      }
+    ],
+    image: Right,
+    source_code_link: "http://rightbraininfotech.in/",
+  }, {
+    name: "Travel Classified",
+    description:
+    "Travel Classified is a comprehensive Hajj and Umrah inquiry portal designed to  of travel package providers offering Hajj and Umrah services. ensuring a seamless and hassle-free experience for users.",
+    tasks: 
+       
+    "Converting Design Systems into reusable CSS and Components  Making complex layouts Developing and maintaining the user interfaces  Worked on Optimization, Performance and UX Improvement. Making Responsive UI. Integrating API s and RESTful Services.",
+    tags: [
+      {
+        id: 0,
+        name: "HTML"
+      },
+      {
+        id: 1,
+        name: "CSS"
+      },
+      {
+        id: 2,
+        name: "JavaScript"
+      },
+      {
+        id: 3,
+        name: "React"
+      },
+      {
+        id: 3,
+        name: "Ionic"
+      },
+      {
+        id: 5,
+        name: "Figma"
+      },
+      {
+        id: 6,
+        name: "Responsive Design"
+      },
+      {
+        id: 7,
+        name: "REST"
+      },
+      {
+        id: 8,
+        name: "Tailwindcss"
+      },
+      {
+        id: 9,
+        name: "Material UI"
+      }
+    ],
+    image: tc,
+    source_code_link: "https://tcp.dimosh.in/",
+  }, {
+    name: "Job Done",
+    description:
+    "Job Done is an all-inclusive cleaning service website dedicated to providing top-notch house cleaning and related services. Our platform connects users with professional cleaning service providers, ensuring a clean, healthy, and comfortable living environment.",
+    tasks: 
+       
+    "Converting Design Systems into reusable CSS and Components  Making complex layouts Developing and maintaining the user interfaces  Worked on Optimization, Performance and UX Improvement. Making Responsive UI. Integrating API s and RESTful Services.",
+    tags: [
+      {
+        id: 0,
+        name: "HTML"
+      },
+      {
+        id: 1,
+        name: "CSS"
+      },
+      {
+        id: 2,
+        name: "JavaScript"
+      },
+      {
+        id: 3,
+        name: "React"
+      },
+      {
+        id: 3,
+        name: "Ionic"
+      },
+      {
+        id: 5,
+        name: "Figma"
+      },
+      {
+        id: 6,
+        name: "Responsive Design"
+      },
+      {
+        id: 7,
+        name: "REST"
+      },
+      {
+        id: 8,
+        name: "Tailwindcss"
+      },
+      {
+        id: 9,
+        name: "Material UI"
+      }
+    ],
+    image: job,
+    source_code_link: "https://mysamplewebsite.in/",
+  },
+  {
+    name: "Edgeon Services",
+    description:
+    "Edge On Services is an innovative online platform specializing in contingency service hire, connecting businesses and individuals with skilled professionals for on-demand tasks. Edge On Services provides a seamless solution for hiring contingent workers",
+    tasks: 
+       
+    "Converting Design Systems into reusable CSS and Components  Making complex layouts Developing and maintaining the user interfaces  Worked on Optimization, Performance and UX Improvement. Making Responsive UI. Integrating API s and RESTful Services.",
+    tags: [
+      {
+        id: 0,
+        name: "HTML"
+      },
+      {
+        id: 1,
+        name: "CSS"
+      },
+      {
+        id: 2,
+        name: "JavaScript"
+      },
+      {
+        id: 3,
+        name: "React"
+      },
+      {
+        id: 3,
+        name: "Ionic"
+      },
+      {
+        id: 5,
+        name: "Figma"
+      },
+      {
+        id: 6,
+        name: "Responsive Design"
+      },
+      {
+        id: 7,
+        name: "REST"
+      },
+      {
+        id: 8,
+        name: "Tailwindcss"
+      },
+      {
+        id: 9,
+        name: "Material UI"
+      }],
+    image: ez,
+    source_code_link: "https://edgeonservices.com/",
+  },
+ 
+];
+
+export { services, technologies, experiences, testimonials, projects };
